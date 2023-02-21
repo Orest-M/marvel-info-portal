108f32582d9b9af2f6662202b9a95471582f8ea2
Orest-M <orestmikhal@gmail.com>
2023-02-21 17:07:32 +0200

    create app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/App';
+// import MarverService from './services/MarverService';
+
+import './style/style.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
